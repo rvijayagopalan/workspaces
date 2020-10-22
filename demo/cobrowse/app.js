b6a8748c529c6ade3d33907b11d0768fc7cd4eee
Vijay Raveendran <vraveendran@deltadentalmi.com>
2020-10-22 19:22:00 -0400

    Working Demo Version 1

---
+++ demo/cobrowse/app.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app).listen(3001);
+var io = require('socket.io').listen(server);
+io.set('log level', 1);
+io.set('transports', ['websocket', 'polling']);
+
+app.use(express.static(__dirname + '/public'));
+
+io.sockets.on('connection', function(socket) {
+    socket.on('CreateSession', function(msg){
+        socket.join(msg);
+    });
+    socket.on('PageChange', function(msg){
+        socket.join(msg);
+        io.sockets.in(msg).emit('SessionStarted', '');
+        console.log('PageChange');
+    });
+    socket.on('JoinRoom', function(msg){
+        socket.join(msg);
+        io.sockets.in(msg).emit('SessionStarted', '');
+    });
+    socket.on('ClientMousePosition', function(msg){
+        socket.broadcast.to(socket.room).emit('ClientMousePosition', {PositionLeft:msg.PositionLeft, PositionTop:msg.PositionTop});
+    });
+    socket.on('AdminMousePosition', function(msg){
+        socket.broadcast.to(msg.room).emit('AdminMousePosition', {PositionLeft:msg.PositionLeft, PositionTop:msg.PositionTop});
+    });
+    socket.on('changeHappened', function(msg){
+        socket.broadcast.to(msg.room).emit('changes', msg.change);
+    });
+    socket.on('DOMLoaded', function(msg){
+        socket.broadcast.to(msg.room).emit('DOMLoaded', '');
+    });
+});
+app.listen(3000);
